c3a4680edb6e6901a66000f2d5976540a4506f6b
ono-itf <ono.masahiro@itfllc.co.jp>
2023-11-01 15:42:00 +0900

    edit, deleteなどの処理を追加

--- src/main/java/com/example/fullstackbackend/controller/UserController.java
+++ src/main/java/com/example/fullstackbackend/controller/UserController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,10 +33,34 @@ public class UserController {
 		return userRepository.findAll();
 	}
 	
+	// 指定のidのデータを取得、なければ例外をスローしている
 	@GetMapping("/user/{id}")
 	User getUserById(@PathVariable Long id) {
 		return userRepository.findById(id)
 				.orElseThrow(()->new UserNotFoundException(id));
+	}
+	
+	
+	@PutMapping("/user/{id}")
+	User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+		return userRepository.findById(id)
+				.map(user -> {
+					// ここでインプットされた情報をセットしなおし
+					user.setUsername(newUser.getUsername());
+					user.setName(newUser.getName());
+					user.setEmail(newUser.getEmail());
+					//ここのsaveで保存している
+					return userRepository.save(user);
+				}).orElseThrow(()->new UserNotFoundException(id));
+	}
+	
+	@DeleteMapping("/user/{id}")
+	String deleteUser(@PathVariable Long id) {
+		if(!userRepository.existsById(id)) {
+			throw new UserNotFoundException(id);
+		}
+		userRepository.deleteById(id);
+		return "User with id No." + id + "has been deleted success.";
 	}
 	
 	
--- src/main/java/com/example/fullstackbackend/exception/UserNotFoundException.java
+++ src/main/java/com/example/fullstackbackend/exception/UserNotFoundException.java
@@ -3,6 +3,6 @@ package com.example.fullstackbackend.exception;
 public class UserNotFoundException extends RuntimeException {
 	
 	public UserNotFoundException(Long id) {
-		super("Could not find the user with id");
+		super("Could not find the user with id No." + id);
 	}
 }
